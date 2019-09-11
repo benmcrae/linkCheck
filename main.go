a342bdf694c8578b46308eb0fa5caffbe5dc94ef
Ben McRae <a-bmcrae@hotels.com>
2019-09-11 17:39:59 +0100

    channels half working

--- main.go
+++ main.go
@@ -13,15 +13,22 @@ func main() {
 		"http://www.github.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
 	}
+
+	fmt.Println(<-c)
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "Migh be down!"
+		return
 	}
 	fmt.Println(link, "is up!")
+	c <- "Might be up!"
 }
